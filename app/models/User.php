03a83d74696e2efe4999f363f7a308de1e6bb7c9
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-08 10:38:44 +0430

    + User model updated

--- app/models/User.php
+++ app/models/User.php
@@ -1,17 +1,17 @@
 <?php 
 
-namespace app\models;
+namespace app\app\models;
 
 use app\core\Application;
 use app\core\Model;
 
 class User extends Model
 {
-    public $id = null;
+    public ?int $id = null;
     public string $name = '';
     public string $username = '';
     public string $email = '';
-    public int $type = 0;
+    public int $type = 1;
     public float $credit = 0;
     public bool $is_active = true;
     public int $num_files = 0;
@@ -25,5 +25,11 @@ class User extends Model
     public function attributes(): array
     {
         return ['id', 'name', 'username', 'email', 'type', 'credit', 'is_active', 'num_files', 'password'];
+    }
+
+    public function add(): bool
+    {
+        $this->password = sha1($this->password);
+        return parent::add();
     }
 }
