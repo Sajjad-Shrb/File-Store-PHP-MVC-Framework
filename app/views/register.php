f2073a2a986b5f11b79d1c5b9e340fcf9a0cc5e0
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-08 10:40:44 +0430

    Register form updated

--- app/views/register.php
+++ app/views/register.php
@@ -1,17 +1,22 @@
 <form method = "post" action="">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" aria-describedby="name" name="Name">
+        <input type="text" class="form-control" id="name" aria-describedby="name" name="name">
     </div>    
     
     <div class="mb-3">
+        <label for="username" class="form-label">UserName</label>
+        <input type="text" class="form-control" id="username" aria-describedby="username" name="username">
+    </div>  
+
+    <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" aria-describedby="email" name="Email">
+        <input type="email" class="form-control" id="email" aria-describedby="email" name="email">
     </div>
 
     <div class="mb-3">
-        <label for="Password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="Password" name="Password">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password">
     </div>
 
     <div class="mb-3 form-check">
