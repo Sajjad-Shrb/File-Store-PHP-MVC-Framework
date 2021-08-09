9845849d282d1b0800f01a7f3c0a5462fd4c4879
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-09 16:11:07 +0430

    + Add Authentication before show home content

--- app/views/home.php
+++ app/views/home.php
@@ -1,13 +1,3 @@
 <?php
 
-use app\core\Application;
-
-$id =       Application::$app->session->get('id');
-$username = Application::$app->session->get('username');
-$name =     Application::$app->session->get('name');
-$email =    Application::$app->session->get('email');
-
-if ($id)
-    echo "You are logged in. your mail is: $email |  <a href=\"/logout\">Logout</a>";
-else
-    echo "You are Guest! | <a href=\"/login\">Login</a> or <a href=\"/register\">Register</a>" ;
+echo "<br>Home";
