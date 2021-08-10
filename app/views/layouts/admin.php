f03a33908e9594927dfa053508a7f8952f3cf631
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-10 10:55:02 +0430

    + Add admin panel layout

---
+++ app/views/layouts/admin.php
@@ -0,0 +1,65 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="/css/admin.css">
+  </head>
+  <body><div class="area"></div><nav class="main-menu">
+            <ul>
+                <li>
+                    <a href="http://justinfarrow.com">
+                        <i class="fa fa-home fa-2x"></i>
+                        <span class="nav-text">
+                            داشبورد
+                        </span>
+                    </a>
+                  
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                        <i class="fa fa-upload fa-2x"></i>
+                        <span class="nav-text">
+                            تنظیمات آپلود
+                        </span>
+                    </a>
+                    
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                       <i class="fa fa-user fa-2x"></i>
+                        <span class="nav-text">
+                            مدیریت کاربران
+                        </span>
+                    </a>
+                    
+                </li>
+                <li class="has-subnav">
+                    <a href="#">
+                       <i class="fa fa-folder-open fa-2x"></i>
+                        <span class="nav-text">
+                            مدیریت فایل‌ها
+                        </span>
+                    </a>
+                   
+                </li>
+                <li>
+                   <a href="#">
+                       <i class="fa fa-dollar fa-2x"></i>
+                        <span class="nav-text">
+                            مدیریت معاملات
+                        </span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="logout">
+                <li>
+                   <a href="<?php echo 'logout'; ?>">
+                         <i class="fa fa-power-off fa-2x"></i>
+                        <span class="nav-text">
+                            Logout
+                        </span>
+                    </a>
+                </li>  
+            </ul>
+        </nav>
+  </body>
+    </html>
