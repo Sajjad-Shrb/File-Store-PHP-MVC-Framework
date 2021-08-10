d5717cba8d26c8cf9212c6a599c5f8c3fdbf173c
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-10 10:53:04 +0430

    + Add showAdminPage function

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,11 +3,28 @@
 namespace app\app\controllers;
 
 use app\core\Controller;
+use app\core\Request;
 
 class AdminController extends Controller
-{   
-    public function index()
-    {
+{
+    public ?array $requests;
 
+    public function index(Request $request)
+    {
+        $this->requests = $request->getBody();
+
+        if ($this->path == '/admin' && $this->method == 'get')
+            $this->action = 'showAdminPage';
+        elseif ($this->path == '/admin' && $this->method == 'post')
+            $this->action = 'admin';
+
+        return call_user_func([__CLASS__, $this->action]);
     }
+
+    public function showAdminPage()
+    {
+        $this->setLayout('admin');
+        return $this->render('admin');
+    }
+
 }
