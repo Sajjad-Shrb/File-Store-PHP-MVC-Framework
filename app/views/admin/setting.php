5440ba4c03c1d32b4fb8bed429e4c074f6a33ee0
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-11 22:30:11 +0430

    + Add Setting View into admin panel

---
+++ app/views/admin/setting.php
@@ -0,0 +1,67 @@
+<style>
+	body {
+		margin-left: 70px;
+	}
+</style>
+
+<div>
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th scope="col">شناسه</th>
+				<th scope="col">ویژگی</th>
+				<th scope="col">مقدار</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php for ($i = 0; $i < $rows_count; $i++) { ?>
+				<tr>
+					<form action="/admin/setting" method="post">
+						<input type="hidden" name="key_name" value="<?php echo $rows[$i]['key_name']; ?>" />
+						<th scope="row">
+							<?php echo $i; ?>
+						</th>
+						<td>
+							<?php echo $rows[$i]['name']; ?>
+						</td>
+						<td>
+							<?php if (is_array($rows[$i]['value'])) : ?>
+								<?php echo json_decode($rows[$i]['value']); ?>
+							<?php else: ?>
+								<?php echo $rows[$i]['value']; ?>
+							<?php endif; ?>
+							<br>
+
+							<p style="margin-top: 8px !important;">
+								<button type="button" value="ویرایش" style="background: none; border: none; color: blue;text-decoration: none; cursor: pointer;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">ویرایش</button>
+							</p>
+
+
+							<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h5 class="modal-title" id="exampleModalLabel">توجه</h5>
+											<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+										</div>
+										<div class="modal-body">
+												<div class="mb-3">
+													<label for="recipient" class="col-form-label">لطفا مقدار را وارد کنید:</label>
+													<input type="text" class="form-control" id="recipient" name="value" placeholder="<?php echo $rows[$i]['value']; ?>">
+
+												</div>
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">بستن</button>
+											<button type="submit" class="btn btn-primary">ارسال</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</td>
+					</form>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+</div>
