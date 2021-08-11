f14bf113859a7142bcfd31cd8f42dd5210f147ef
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-11 08:48:18 +0430

    + Add update feature for Admins

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace app\app\controllers;
 
+use app\app\models\User;
 use app\core\Controller;
 use app\core\Request;
 
@@ -11,20 +12,46 @@ class AdminController extends Controller
 
     public function index(Request $request)
     {
+        $this->setLayout('admin');
+
         $this->requests = $request->getBody();
 
         if ($this->path == '/admin' && $this->method == 'get')
-            $this->action = 'showAdminPage';
+            $this->action = 'showAdminDashboardPage';
         elseif ($this->path == '/admin' && $this->method == 'post')
             $this->action = 'admin';
-
+        elseif ($this->path == '/admin/users' && $this->method == 'get')
+            $this->action = 'showManageUsersPage';
+        elseif ($this->path == '/admin/users' && $this->method == 'post')
+            $this->action = 'update';
+            
         return call_user_func([__CLASS__, $this->action]);
     }
 
-    public function showAdminPage()
+    public function showAdminDashboardPage()
     {
-        $this->setLayout('admin');
-        return $this->render('admin');
+        return $this->render('admin/dashboard');
     }
 
+    public function showManageUsersPage()
+    {       
+        return $this->render('admin/users');
+    }
+
+    //Active, Deactive and ChangeAccessLevel
+    public function update()
+    {
+        $data = $this->requests;
+
+        var_dump($data);
+        exit;
+        $where = [
+            'id' => intval($data['id'])
+        ];
+        unset($data['id']);
+
+        $user = new User();
+
+        return $user->update($data, $where);
+    }
 }
