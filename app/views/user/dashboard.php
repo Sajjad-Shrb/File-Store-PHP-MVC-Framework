2d9f5ba23620fe0fa7f6923f747680e8d805d3cf
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-16 01:10:26 +0430

    Add user dashboard

---
+++ app/views/user/dashboard.php
@@ -0,0 +1,5 @@
+<div style="direction: rtl;">
+    <p><?php echo $name; ?> عزیز خوش آمدی</p>
+    <p>اعتبار شما: <?php echo $credit; ?></p>
+    <p>تعداد فایل های آپلود شده توسط شما: <?php echo $num_files; ?> </p>
+</div>
