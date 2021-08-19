b1229dc385f5aadf5fb5549459e33fbb08a7d0cc
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-19 19:39:16 +0430

    + Add put, delete method and + Add middleware funcs

--- core/Router.php
+++ core/Router.php
@@ -2,7 +2,7 @@
 
 namespace app\core;
 
-use app\app\controllers\UserController;
+use Exception;
 
 class Router
 {
@@ -16,31 +16,45 @@ class Router
         $this->response = $response;
     }
 
-    public function routing($path, $callback)
+    public function get($path, $callback)
     {
-        $callback = [$callback, 'index'];
-
         $path = rtrim($path, '/');
 
-        $this->routes[$path] = $callback;
+        $this->routes["get"][$path] = $callback;
     }
 
-    // public function get($path, $callback)
-    // {
-    //     $this->routes["get"][$path] = $callback;
-    // }
+    public function post($path, $callback)
+    {
+        $path = rtrim($path, '/');
 
-    // public function post($path, $callback)
-    // {
-    //     $this->routes["post"][$path] = $callback;
-    // }
+        $this->routes["post"][$path] = $callback;
+    }
+
+    public function put($path, $callback)
+    {
+        $path = rtrim($path, '/');
+
+        $this->routes["put"][$path] = $callback;
+    }
+
+    public function delete($path, $callback)
+    {
+        $path = rtrim($path, '/');
+
+        $this->routes["delete"][$path] = $callback;
+    }
+
+    public function render($callback, $params = [])
+    {
+        return Application::$app->view->render($callback, $params);
+    }
 
     public function resolve()
     {
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
 
-        $callback = $this->routes[$path] ?? false;
+        $callback = $this->routes[$method][$path] ?? false;
 
         if($callback === false) {
             $this->response->setStatusCode(404);
@@ -49,18 +63,32 @@ class Router
             return $this->response->render();
         }
 
-        // elseif (is_string($callback)){
-        //     return $this->renderView($callback);
-        // }
+        elseif (is_string($callback)){
+            return $this->render($callback);
+        }
 
         elseif(is_array($callback)) {
-
-            $callback[0] = new $callback[0];
+            $callback[0] = new $callback[0]($callback[1]);
             $controller = $callback[0];
             
             $controller->path = $path;
             $controller->method = $method;
+            $controller->action = $callback[1];
 
+            Application::$app->controller = $controller;
+
+            $middlewares = $controller->getMiddlewares();
+            foreach ($middlewares as $middleware) {
+                try {
+                    $middleware->execute();
+                } catch(\Exception $e) {
+                    $this->response->setStatusCode($e->getCode());
+                    $this->response->setStatusMessage($e->getMessage());
+        
+                    return $this->response->render();
+                }
+            }
+            
             return call_user_func($callback, $this->request);
         }
 
