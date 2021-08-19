3d465a4325ab7e937381bb053788ff2da51e5358
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-19 19:36:50 +0430

    Improved code order

--- core/Application.php
+++ core/Application.php
@@ -6,29 +6,28 @@ use app\app\models\User;
 
 class Application
 {
-    public static $root_dir;
     public static Application $app;
+    public static $ROOT_DIR;
+    public String $layout = 'main';
     
-    public Controller $controller;
-    public Database $db;
     public Request $request;
     public Response $response;
     public Router $router;
-    public String $layout = 'main';
-    public View $view;
+    public ?Controller $controller = null;
+    public Database $db;
     public Session $session;
+    public View $view;
     public ?User $user = null;
 
     public function __construct($root_dir)
     {
-        static::$root_dir = $root_dir;
         static::$app = $this;
-
-        $this->controller = new Controller();
-        $this->db = new Database();
+        static::$ROOT_DIR = $root_dir;
+        
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+        $this->db = new Database();
         $this->session = new Session();
         $this->view = new View();
         $this->user = new User();
