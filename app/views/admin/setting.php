13851d0adc473519a2d9b8e50f8149b730a0ebac
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-19 19:35:39 +0430

    Code was cleaned

--- app/views/admin/setting.php
+++ app/views/admin/setting.php
@@ -1,3 +1,11 @@
+<?php 
+
+	// echo "<pre>";
+	// var_dump($rows);	
+	// echo "</pre>";
+?>
+
+
 <style>
 	body {
 		margin-left: 70px;
@@ -13,10 +21,12 @@
 				<th scope="col">مقدار</th>
 			</tr>
 		</thead>
+
 		<tbody>
 			<?php for ($i = 0; $i < $rows_count; $i++) { ?>
 				<tr>
 					<form action="/admin/setting" method="post">
+						<input type="hidden" name="_method" value="put">
 						<input type="hidden" name="key_name" value="<?php echo $rows[$i]['key_name']; ?>" />
 						<th scope="row">
 							<?php echo $i; ?>
@@ -25,19 +35,28 @@
 							<?php echo $rows[$i]['name']; ?>
 						</td>
 						<td>
-							<?php if (is_array($rows[$i]['value'])) : ?>
-								<?php echo json_decode($rows[$i]['value']); ?>
-							<?php else: ?>
-								<?php echo $rows[$i]['value']; ?>
-							<?php endif; ?>
+							<?php 
+								switch ($rows[$i]['key_name']) {
+									case 'life_time':
+										$value = $rows[$i]['value'] . ' ثانیه';
+										break;
+									case 'max_size':
+										$value = $rows[$i]['value'] . ' مگابایت';
+										break;
+									default:
+										$value = $rows[$i]['value'];
+										break;
+								}
+							?>
+							<?php echo $value; ?>
 							<br>
-
+								
 							<p style="margin-top: 8px !important;">
-								<button type="button" value="ویرایش" style="background: none; border: none; color: blue;text-decoration: none; cursor: pointer;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">ویرایش</button>
+								<button type="button" value="ویرایش" style="background: none; border: none; color: blue;text-decoration: none; cursor: pointer;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#<?php echo $rows[$i]['key_name']; ?>">ویرایش</button>
 							</p>
 
 
-							<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div class="modal fade" id="<?php echo $rows[$i]['key_name']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 								<div class="modal-dialog">
 									<div class="modal-content">
 										<div class="modal-header">
@@ -48,7 +67,6 @@
 												<div class="mb-3">
 													<label for="recipient" class="col-form-label">لطفا مقدار را وارد کنید:</label>
 													<input type="text" class="form-control" id="recipient" name="value" placeholder="<?php echo $rows[$i]['value']; ?>">
-
 												</div>
 										</div>
 										<div class="modal-footer">
