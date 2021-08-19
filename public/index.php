a6fec1668c6694b1df049112b3c9ba2d5c00d0ce
Sajjad <msajjad.shahrabi@gmail.com>
2021-08-19 19:40:16 +0430

    Improve routing by adding get, post, put & delete methods

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use app\app\controllers\AdminController;
-use app\app\controllers\FileController;
+use app\app\controllers\AuthController;
 use app\app\controllers\HomeController;
 use app\app\controllers\UploadController;
 use app\app\controllers\UserController;
@@ -13,52 +13,49 @@ $app = new Application(dirname(__DIR__));
 
 //TODO: Use root path
 
-$app->router->routing("/", HomeController::class);
-$app->router->routing("/home", HomeController::class);
-$app->router->routing("/home/index", HomeController::class);
+$app->router->get("/", [HomeController::class, 'index']);
+$app->router->get("/home", [HomeController::class, 'index']);
+$app->router->get("/index", [HomeController::class, 'index']);
+$app->router->get("/home/index", [HomeController::class, 'index']);
 
-$app->router->routing("/register", UserController::class);
-$app->router->routing("/login", UserController::class);
-$app->router->routing("/logout", UserController::class);
-$app->router->routing("/user/register", UserController::class);
-$app->router->routing("/user/login", UserController::class);
-$app->router->routing("/user/logout", UserController::class);
+$app->router->get("/register", [AuthController::class, 'showRegisterForm']);
+$app->router->get("/user/register", [AuthController::class, 'showRegisterForm']);
 
-$app->router->routing("/user", UserController::class);
-$app->router->routing("/user/dashboard", UserController::class);
-$app->router->routing("/user/profile", UserController::class);
-$app->router->routing("/user/files", UserController::class);
+$app->router->post("/register", [AuthController::class, 'register']);
+$app->router->post("/user/register", [AuthController::class, 'register']);
 
+$app->router->get("/login", [AuthController::class, 'showLoginForm']);
+$app->router->get("/user/login", [AuthController::class, 'showLoginForm']);
+
+$app->router->post("/login", [AuthController::class, 'login']);
+$app->router->post("/user/login", [AuthController::class, 'login']);
+
+$app->router->get("/user/logout", [AuthController::class, 'logout']);
+$app->router->get("/logout", [AuthController::class, 'logout']);
+
+$app->router->get("/user", [UserController::class, 'showUserDashboardPage']);
+$app->router->get("/user/dashboard", [UserController::class, 'showUserDashboardPage']);
+
+$app->router->get("/user/profile", [UserController::class, 'showProfilePage']);
+$app->router->put("/user/profile", [UserController::class, 'editProfile']);
+
+$app->router->get("/user/files", [UserController::class, 'showFilesPage']);
 
 (new UserController) -> provideUserProfileRoutes();
 
-$app->router->routing("/upload", UploadController::class);
+$app->router->get("/upload", [UploadController::class, 'showUploadPage']);
+$app->router->post("/upload", [UploadController::class, 'upload']);
 
-$app->router->routing("/admin", AdminController::class);
-$app->router->routing("/admin/users", AdminController::class);
-$app->router->routing("/admin/update", AdminController::class);
-$app->router->routing("/admin/setting", AdminController::class);
+$app->router->get("/admin", [AdminController::class, 'showAdminDashboardPage']);
 
-$app->router->routing("/admin/files", FileController::class);
+$app->router->get("/admin/users", [AdminController::class, 'showManageUsersPage']);
+$app->router->put("/admin/users", [AdminController::class, 'updateUsers']);
 
+$app->router->get("/admin/setting", [AdminController::class, 'showSettingPage']);
+$app->router->put("/admin/setting", [AdminController::class, 'config']);
 
-//TODO: implement non-automatic routing
-// $app->router->routing("/register", 'register');
-
-// $app->router->get("/register", [SiteController::class, "register"]);
-// $app->router->post("/register", [SiteController::class, "register"]);
-
-// $app->router->get("/login", [SiteController::class, "login"]);
-// $app->router->post("/login", [SiteController::class, "login"]);
-
-// $app->router->get("/logout", [SiteController::class, "logout"]);
-
-// $app->router->get("/reserve", [UserController::class, "reserve"]);
-
-// $app->router->get("/admin", [UserController::class, "admin"]);
-// $app->router->post("/admin", [UserController::class, "admin"]);
-
-// $app->router->get("/profile", [UserController::class, "profile"]);
-// $app->router->post("/profile", [UserController::class, "profile"]);
+$app->router->get("/admin/files", [AdminController::class, 'showFilesPage']);
+$app->router->put("/admin/files", [AdminController::class, 'verifyFiles']);
+$app->router->delete("/admin/files", [AdminController::class, 'deleteFiles']);
 
 $app->run();
